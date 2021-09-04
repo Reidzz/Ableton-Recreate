1c63e8d0a50e5ce93281f58d4e2d80b5d33c3a50
Reidzz <danielreid2007@hotmail.co.uk>
2021-09-04 13:02:49 +0100

    Updated files

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const moreLink = document.querySelector(".dropdown");
 const moreNavSection = document.querySelector(".more-section");
+const mobileNavLink = document.querySelector(".mobile-nav-link");
+const mobileNav = document.querySelector(".mobile-nav");
 
 moreLink.addEventListener("click", function() {
     moreLink.classList.toggle("active");
     moreNavSection.classList.toggle("open");
 });
+
+mobileNavLink.addEventListener("click", function() {
+    mobileNav.classList.toggle("open");
+});
+
